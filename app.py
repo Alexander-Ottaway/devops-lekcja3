a39bc3cf98b204373cc4bf50ebcce756fb8d914a
Alexander-Ottaway <alexander.ottaway@microsoft.wsei.edu.pl>
2025-11-20 18:13:32 +0100

    Add Docker Compose and Redis counter

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask
+import redis
+import os
 
 app = Flask(__name__)
 
+redis_host = os.getenv("REDIS_HOST", "localhost")
+r = redis.Redis(host=redis_host, port=6379, db=0)
+
 @app.get("/")
 def hello():
-	return {"message": "Hello, world!"}
+    r.incr("visits")
+    visits = r.get("visits").decode("utf-8")
+
+    return {
+        "message": "Hello, world!",
+        "visits": visits
+    }
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
